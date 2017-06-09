69fe1979e33c3242ef049d9c41948bce2ea35e7b
Sol Robinson <solrobinson@Sols-Mac.local>
2017-06-09 10:37:25 -0700

    Initial Commit

---
+++ BWMonitor/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  BWMonitor
+//
+//  Created by Sol Robinson on 6/9/17.
+//  Copyright © 2017 NCSA. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
